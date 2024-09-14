bb27e4d4f60803a4e7ab826257f1df4769e6e45a
Niti Dagar <nitid2207@gmail.com>
2024-09-14 22:30:00 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
 path(route='register', view=views.registration, name='register'),
     # path for login
 path(route='login', view=views.login_user, name='login'),
-
-path(route='logout', view=views.logout_request, name='logout'),
+    # path for logout
+path(route='logout', view=views.logout_request, name='logout'),    
     # path for dealer reviews view
 
     # path for add a review view
