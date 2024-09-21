5f3054f3c65fd3f1d2545e41d5df2d5993deeab3
Niti Dagar <nitid2207@gmail.com>
2024-09-21 12:39:00 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,4 @@
-# Uncomment the imports before you add the code
+#Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
@@ -12,6 +12,8 @@ path(route='register', view=views.registration, name='register'),
 path(route='login', view=views.login_user, name='login'),
     # path for logout
 path(route='logout', view=views.logout_request, name='logout'),    
+ 
+path(route='get_cars', view=views.get_cars, name ='getcars'), 
     # path for dealer reviews view
 
     # path for add a review view
